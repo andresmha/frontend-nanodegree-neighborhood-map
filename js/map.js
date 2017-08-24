ed872318b0d150dad380f92339c587f5b8f5ed22
Andres Hernandez <andru.h@gmail.com>
2017-08-23 21:18:01 -0500

    Added map, started introducing data

---
+++ js/map.js
@@ -0,0 +1,35 @@
+//Map funcionality
+var map;
+
+function initMap() {
+	
+
+	map = new google.maps.Map(document.getElementById("map"), {
+		center: {lat: 43.6524174, lng: -79.3926163},
+		zoom: 13,
+		disableDefaultUI: true
+	});
+
+
+	//MARKERS
+	var firstMarker = {lat: 43.678041, lng: -79.4116326};
+
+	var marker = new google.maps.Marker({
+		position: firstMarker,
+		map: map,
+		title: "Casa Loma"
+	});
+
+	//InfoWindow
+	var infoWindow = new google.maps.InfoWindow({
+		content: "Prueba prplknaf asf adsf asdf asdf asdf  asdf sadf sad f asd flnlkdf"
+	});
+
+	//it wont start automatically so ill bind it to a click listener
+	marker.addListener("click", function(){
+		infoWindow.open(map, marker);
+	});
+
+
+}
+
---
+++ js/places_GeoJSONP.js
@@ -0,0 +1,45 @@
+placesfeed_callback({
+	"sites":[
+		{
+			"name": "Casa Loma",
+			"latitude": 43.652417,
+			"longitude": -79.392616
+		},
+		{
+			"name": "",
+			"latitude": ,
+			"longitude": ,
+		},
+		{
+			"name": "",
+			"latitude": ,
+			"longitude": ,
+		},
+		{
+			"name": "",
+			"latitude": ,
+			"longitude": ,
+		},
+		{
+			"name": "",
+			"latitude": ,
+			"longitude": ,
+		},
+		{
+			"name": "",
+			"latitude": ,
+			"longitude": ,
+		},
+		{
+			"name": "",
+			"latitude": ,
+			"longitude": ,
+		},
+		{
+			"name": "",
+			"latitude": ,
+			"longitude": ,
+		}
+	]
+});
+					
